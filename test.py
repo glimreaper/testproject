4ec23d3a7cdfdfbaf296222d11d5c1d6ea0b46cc
glimreaper <limgarretshawn@yahoo.com>
2017-05-20 10:07:35 -0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+print ("hello world")
+name = input("Name? >>>>")
+print (int(name) + 5)
+if name == "26":
+    print("a")
+elif name == "25":
+    print ("c")
+else :
+    print("b")
+
+for (i = 0; i< 4; i+=1):
+    print ("4")
+
